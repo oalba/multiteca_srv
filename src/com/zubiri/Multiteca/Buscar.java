65206c909f22357a0d49f4fa3c406a7ea555da8a
zubiri <zubiri@zubiri-VirtualBox.(none)>
2015-05-15 10:41:36 +0200

    jar-ean sartzen

--- src/com/zubiri/Multiteca/Buscar.java
+++ src/com/zubiri/Multiteca/Buscar.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.zubiri.multiteca.*;
+
 /**
  * Servlet implementation class Buscar
  */
@@ -59,20 +61,37 @@ public class Buscar extends HttpServlet {
 			while (rsl.next()) {
 				out.println("Código de barras: " + rsl.getInt("obras.cod_barras") + "<br>");
 				out.println("Título: " + rsl.getString("obras.titulo") + "<br>");
-				out.println("Autor: " + rsl.getString("obras.autor") + "<br>");
 				out.println("Año de edición: " + rsl.getInt("obras.añoEdicion") + "<br>");
 				out.println("Editorial: " + rsl.getString("libros.editorial") + "<br>");
-				out.println("Número de páginas: " + rsl.getInt("libros.nPaginas") + "<hr>");
-		    }
+				out.println("Número de páginas: " + rsl.getInt("libros.nPaginas") + "<br>");
+				out.println("AUTOR");
+				out.println("Autor: " + rsl.getString("obras.autor") + "<br>");
+				Statement stmta = cone.createStatement();
+				ResultSet rsa = stmta.executeQuery("SELECT * FROM artistas WHERE nombre = '"+rsl.getString("obras.autor")+"'");
+				while (rsa.next()) {
+					out.println("Año de nacimiento: "+rsa.getInt("añoNacimiento")+"<hr>");
+					Artista autor = new Artista(rsl.getString("obras.autor"),rsa.getInt("añoNacimiento"));
+					//Artista autor = new Artista(rsl.getString("obras.autor"),0);
+					Libro libro = new Libro(rsl.getString("obras.titulo"),autor,rsl.getInt("obras.añoEdicion"),rsl.getString("libros.editorial"),rsl.getInt("libros.nPaginas"));
+				}
+			}
 		//} else if (st == "pelicula") {
 			Statement stmtp = cone.createStatement();
 			ResultSet rsp = stmtp.executeQuery("SELECT * FROM obras,peliculas WHERE obras.cod_barras = "+cb+" and obras.cod_barras=peliculas.cod_barras");
 			while (rsp.next()) {
 				out.println("Código de barras: " + rsp.getInt("obras.cod_barras") + "<br>");
 				out.println("Título: " + rsp.getString("obras.titulo") + "<br>");
-				out.println("Autor: " + rsp.getString("obras.autor") + "<br>");
 				out.println("Año de edición: " + rsp.getInt("obras.añoEdicion") + "<br>");
-				out.println("Productora: " + rsp.getString("peliculas.productora") + "<hr>");
+				out.println("Productora: " + rsp.getString("peliculas.productora") + "<br>");
+				out.println("AUTOR");
+				out.println("Autor: " + rsp.getString("obras.autor") + "<br>");
+				Statement stmta = cone.createStatement();
+				ResultSet rsa = stmta.executeQuery("SELECT * FROM artistas WHERE nombre = '"+rsp.getString("obras.autor")+"'");
+				while (rsa.next()) {
+					out.println("Año de nacimiento: "+rsa.getInt("añoNacimiento")+"<hr>");
+					Artista autor = new Artista(rsp.getString("obras.autor"),rsa.getInt("añoNacimiento"));
+					Pelicula pelicula = new Pelicula(rsp.getString("obras.titulo"),autor,rsp.getInt("obras.añoEdicion"),rsp.getString("peliculas.productora"));
+				}				
 		    }
 		//} else {
 			Statement stmtd = cone.createStatement();
@@ -80,10 +99,18 @@ public class Buscar extends HttpServlet {
 			while (rsd.next()) {
 				out.println("Código de barras: " + rsd.getInt("obras.cod_barras") + "<br>");
 				out.println("Título: " + rsd.getString("obras.titulo") + "<br>");
-				out.println("Autor: " + rsd.getString("obras.autor") + "<br>");
 				out.println("Año de edición: " + rsd.getInt("obras.añoEdicion") + "<br>");
 				out.println("Discográfica: " + rsd.getString("discos.discografica") + "<br>");
-				out.println("Número de canciones: " + rsd.getInt("discos.nCanciones") + "<hr>");
+				out.println("Número de canciones: " + rsd.getInt("discos.nCanciones") + "<br>");
+				out.println("AUTOR");
+				out.println("Autor: " + rsd.getString("obras.autor") + "<br>");
+				Statement stmta = cone.createStatement();
+				ResultSet rsa = stmta.executeQuery("SELECT * FROM artistas WHERE nombre = '"+rsd.getString("obras.autor")+"'");
+				while (rsa.next()) {
+					out.println("Año de nacimiento: "+rsa.getInt("añoNacimiento")+"<hr>");
+					Artista autor = new Artista(rsd.getString("obras.autor"),rsa.getInt("añoNacimiento"));
+					Disco disco = new Disco(rsd.getString("obras.titulo"),autor,rsd.getInt("obras.añoEdicion"),rsd.getString("discos.discografica"),rsd.getInt("discos.nCanciones"));
+				}				
 		    }
 		//}
 		out.println("<br/><a href='index.html'><input type='button' value='Volver'></a>");
